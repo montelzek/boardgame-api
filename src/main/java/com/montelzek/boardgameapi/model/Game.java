7b6c48bda45934365bef2333ff01cde77a2fe3af
montelzek <patrykmatras378@gmail.com>
2025-04-11 23:38:07 +0200

    Implemented Game entity

---
+++ src/main/java/com/montelzek/boardgameapi/model/Game.java
@@ -0,0 +1,46 @@
+package com.montelzek.boardgameapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "games")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(name = "min_players", nullable = false)
+    private Integer minPlayers;
+
+    @Column(name = "max_players", nullable = false)
+    private Integer maxPlayers;
+
+    @Column(name = "play_time", nullable = false)
+    private Integer playTime;
+
+    private String publisher;
+
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    @OneToMany(mappedBy = "game", cascade = CascadeType.ALL)
+    private Set<Review> reviews = new HashSet<>();
+
+    @ManyToMany(mappedBy = "games")
+    private Set<User> users = new HashSet<>();
+}
