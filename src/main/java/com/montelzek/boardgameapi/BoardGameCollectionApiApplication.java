d916a1697da6b38c31907867921ba8568e7293e6
montelzek <patrykmatras378@gmail.com>
2025-04-11 19:21:06 +0200

    Initial commit

---
+++ src/main/java/com/montelzek/boardgameapi/BoardGameCollectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.montelzek.boardgameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameCollectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameCollectionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montelzek/boardgameapi/BoardGameCollectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montelzek.boardgameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameCollectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
