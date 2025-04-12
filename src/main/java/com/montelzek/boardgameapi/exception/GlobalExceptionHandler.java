b7175af22434895ecd6e28a5a4087ceefde1c1c3
montelzek <patrykmatras378@gmail.com>
2025-04-12 22:53:12 +0200

    Handled ResponseStatusException

--- src/main/java/com/montelzek/boardgameapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/montelzek/boardgameapi/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,5 +23,13 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handleResponseStatusException(
+            ResponseStatusException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getReason());
+        return new ResponseEntity<>(error, ex.getStatusCode());
     }
 }
