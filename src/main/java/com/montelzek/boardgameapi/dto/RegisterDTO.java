0474579640d23556860a0161778a2c0e76804621
montelzek <patrykmatras378@gmail.com>
2025-04-13 11:38:04 +0200

    refactor: reverting to the previous implementation

--- src/main/java/com/montelzek/boardgameapi/dto/RegisterDTO.java
+++ src/main/java/com/montelzek/boardgameapi/dto/RegisterDTO.java
@@ -5,28 +5,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-public class RegisterDTOs {
+@Data
+public class RegisterDTO {
 
-    @Data
-    public static class RegisterRequest {
-        @NotBlank(message = "Full name is required")
-        private String fullName;
+    @NotBlank(message = "Full name is required")
+    private String fullName;
 
-        @NotBlank(message = "Email is required")
-        @Email(message = "Invalid email format")
-        private String email;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
 
-        @NotBlank(message = "Password is required")
-        @Size(min = 6, message = "Password must be at least 6 characters long")
-        private String password;
-    }
-
-    @Data
-    public static class RegisterResponse {
-        private Long id;
-        private String fullName;
-        private String email;
-        private String password;
-    }
-
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
 }
