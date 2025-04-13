f3c30d8405fa1b60dd92af4ee727c5c2019d0525
montelzek <patrykmatras378@gmail.com>
2025-04-14 01:31:14 +0200

    Added ReviewRepository with findByGameId and findByGaIdAndUserId methods

---
+++ src/main/java/com/montelzek/boardgameapi/repository/ReviewRepository.java
@@ -0,0 +1,16 @@
+package com.montelzek.boardgameapi.repository;
+
+import com.montelzek.boardgameapi.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByGameId(Long gameId);
+
+    Optional<Review> findByGameIdAndUserId(Long gameId, Long userId);
+}
