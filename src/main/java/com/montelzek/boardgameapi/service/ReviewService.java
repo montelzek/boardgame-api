469a0a98b2764ac27ca32ec6e859e19e53d72fe6
montelzek <patrykmatras378@gmail.com>
2025-04-14 01:31:53 +0200

    feat: Define ReviewService interface
    This commit introduces the `ReviewService` interface, which outlines the contract for managing game reviews within the board game API.
    
    The interface defines the following methods:
    
    - `getReviewsByGameId(Long gameId)`: Retrieves and returns a list of reviews associated with a specific game ID as `ReviewDTOs.ReviewResponse` objects.
    - `createReview(Long gameId, ReviewDTOs.ReviewRequest reviewRequest)`: Creates a new review for a specific game (identified by `gameId`) using the data provided in the `ReviewDTOs.ReviewRequest` and returns the created `Review` entity.
    - `updateReview(Long reviewId, ReviewDTOs.ReviewRequest reviewRequest)`: Updates an existing review (identified by `reviewId`) with the data provided in the `ReviewDTOs.ReviewRequest` and returns the updated `Review` entity.
    - `deleteReview(Long reviewId)`: Deletes a review based on its unique ID.

---
+++ src/main/java/com/montelzek/boardgameapi/service/ReviewService.java
@@ -0,0 +1,17 @@
+package com.montelzek.boardgameapi.service;
+
+import com.montelzek.boardgameapi.dto.ReviewDTOs;
+import com.montelzek.boardgameapi.model.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    List<ReviewDTOs.ReviewResponse> getReviewsByGameId(Long gameId);
+
+    Review createReview(Long gameId, ReviewDTOs.ReviewRequest reviewRequest);
+
+    Review updateReview(Long reviewId, ReviewDTOs.ReviewRequest reviewRequest);
+
+    void deleteReview(Long reviewId);
+}
