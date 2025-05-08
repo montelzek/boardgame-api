bce4e619ca2e701a6d533e61510b90319bec3598
montelzek <patrykmatras378@gmail.com>
2025-05-08 18:53:18 +0200

    Added Builder annotation to inner class GameRequest

--- src/main/java/com/montelzek/boardgameapi/dto/GameDTOs.java
+++ src/main/java/com/montelzek/boardgameapi/dto/GameDTOs.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class GameDTOs {
 
     @Data
+    @Builder
     public static class GameRequest {
         @NotBlank(message = "Title is required")
         private String title;
