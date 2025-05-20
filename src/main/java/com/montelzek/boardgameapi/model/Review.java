bd2627c417fd5059db8b2b0704c28c2b60185f3f
montelzek <patrykmatras378@gmail.com>
2025-05-20 23:22:25 +0200

    Added AllArgsConstructor and Builder annotations

--- src/main/java/com/montelzek/boardgameapi/model/Review.java
+++ src/main/java/com/montelzek/boardgameapi/model/Review.java
@@ -1,9 +1,7 @@
 package com.montelzek.boardgameapi.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -13,6 +11,8 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Review {
 
     @Id
