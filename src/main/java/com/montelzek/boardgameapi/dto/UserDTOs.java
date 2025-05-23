c46eae5de9385a856b7e08de66a61c3cbcd9dbea
montelzek <patrykmatras378@gmail.com>
2025-05-23 22:42:28 +0200

    Added Builder annotation for UserUpdateRequest

--- src/main/java/com/montelzek/boardgameapi/dto/UserDTOs.java
+++ src/main/java/com/montelzek/boardgameapi/dto/UserDTOs.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class UserDTOs {
 
     @Data
+    @Builder
     public static class UserUpdateRequest {
         @NotBlank(message = "Full name is required")
         private String fullName;
