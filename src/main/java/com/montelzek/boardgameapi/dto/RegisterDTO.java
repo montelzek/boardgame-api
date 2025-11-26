fc88dc68fe6ede5b55676035d3c46d36c26cbea9
montelzek <patrykmatras378@gmail.com>
2025-11-26 01:38:05 +0100

    refactor: changed dto class to record

--- src/main/java/com/montelzek/boardgameapi/dto/RegisterDTO.java
+++ src/main/java/com/montelzek/boardgameapi/dto/RegisterDTO.java
@@ -3,19 +3,17 @@ package com.montelzek.boardgameapi.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
-@Data
-public class RegisterDTO {
+public record RegisterDTO(
+        @NotBlank(message = "Full name is required")
+        @Size(max = 100, message = "Full name must be less than 100 characters long")
+        String fullName,
 
-    @NotBlank(message = "Full name is required")
-    private String fullName;
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
-    private String email;
-
-    @NotBlank(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters long")
-    private String password;
-}
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, message = "Password must be at least 6 characters long")
+        String password
+) {}
